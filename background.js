401ca61618a391a7d1c7979dd9b3dad38b276885
royalpinto007 <royalpinto007@gmail.com>
2025-12-22 15:31:02 +0530

    feat: dynamic website even on change

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+// Open side panel when extension icon is clicked
 chrome.runtime.onInstalled.addListener(() => {
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
+
+// Listen for tab changes
+chrome.tabs.onActivated.addListener(async () => {
+  notifySidePanel();
+});
+
+chrome.tabs.onUpdated.addListener(async (_, changeInfo) => {
+  if (changeInfo.status === "complete") {
+    notifySidePanel();
+  }
+});
+
+function notifySidePanel() {
+  chrome.runtime.sendMessage({
+    type: "TAB_CHANGED"
+  });
+}
