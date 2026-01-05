5968f710e8eeb4402e42ab72bf7f85d0cf54872c
Md Sahil <md61421@gmail.com>
2026-01-05 13:34:24 +0530

    feat: remove dropdown

--- sidepanel.js
+++ sidepanel.js
@@ -618,12 +618,6 @@ function renderSavedItem(item) {
         </svg>
       </button>
 
-      <div class="toggle-icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <polyline points="6 9 12 15 18 9"></polyline>
-        </svg>
-      </div>
-
       <button class="delete-saved-btn" title="Remove">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
           stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
